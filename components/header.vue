74b8c2b95e716422349342e1f78383b20751f6ed
nemurin <yh.kyud.28@gmail.com>
2022-01-02 11:22:14 +0900

    add capricorn.vue

--- components/header.vue
+++ components/header.vue
@@ -3,6 +3,7 @@
     <li class="selection"><router-link to="/">index</router-link></li>
     <li class="selection"><router-link to="/other">other</router-link></li>
     <li class="selection"><router-link to="/sample">sample</router-link></li>
+    <li class="selection"><router-link to="/capricorn">カプリコーン杯</router-link></li>
   </ul>
 </template>
 <script>
@@ -13,5 +14,18 @@
 <style>
 li {
     list-style: none;
+    position: absolute;
+    top: 50%;
+    margin-top: -10px;
+    content: '';
+    border-left: 1px solid #f2f2f2;
+    position: relative;
+    display: block;
+    float: left;
+    width: 109px;
+    height: 12px;
+    font-size: 12px;
+    text-align: center;
+    cursor: pointer;
 }
 </style>
--- pages/capricorn.vue
+++ pages/capricorn.vue
@@ -81,26 +81,11 @@ export default {
   data: function(){
     return {
       items: [
-       { Title1: '積み上げた本当のこと',
-      Choices1: '君の力を見せてあげよう!',
-      Content1: 'パワー＋10,スキルPt＋15',
-      Choices2: '雨なんて吹き飛ばしてあげよう!',
-      Content2: '「道悪○」のヒント＋1,スキルPt＋15' },
-       { Title1: 'マッスルジェラシー',
-      Choices1: '更に限界まで鍛えよう!',
-      Content1: '根性+10',
-      Choices2: 'まずは理論を教えてあげよう!',
-      Content2: '賢さ+10' },
-      { Title1: '本当の爽やかさ',
-      Choices1: '演技じゃなく本物を見せるとか',
-      Content1: 'スタミナ+20',
-      Choices2: 'もっと躍動感を見せてみるとか',
-      Content2: 'パワー+20' },
-      { Title1: '周りから見た勝負服姿',
-      Choices1: '魅力というのは積み重ねだ',
-      Content1: '根性+20',
-      Choices2: '君は充分に憧れの存在なんだ',
-      Content2: 'スピード+20' },
+       { Title1: '高松宮記念',
+      Choices1: '条件',
+      Content1: '冬/中京/芝重/1200m(短距離)/左雪',
+      Choices2: '考えたこと',
+      Content2: 'エル→バクシン、オグリ→エアグル' },
      ]
   }
   }
