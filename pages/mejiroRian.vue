85b6abcf7a6bd6c43b2396686617b44e91445f67
nemurin <yh.kyud.28@gmail.com>
2022-02-09 00:12:51 +0900

    standardization css
    Signed-off-by: nemurin <yh.kyud.28@gmail.com>

--- pages/mejiroRian.vue
+++ pages/mejiroRian.vue
@@ -2,7 +2,7 @@
 <div>
   <Header />
 <div v-for="item in items"
-       :key="item.Title1">
+       :key="item.Title1" class="condition">
    <h4>{{item.Title1}}</h4>
    <table>
   <tbody>
@@ -20,60 +20,7 @@
 </div>
 </template>
 <style>
-body {
-    margin: 0;
-    padding: 0;
-    color: #444;
-    font-size: 14px;
-    font-family: Hiragino Kaku Gothic Pro,"ヒラギノ角ゴ Pro W3",Meiryo,"メイリオ",Osaka,MS PGothic,arial,helvetica,sans-serif;
-    line-height: 1.6;
-    background: #FFF;
-}
-li {
-    list-style: none;
-}
-
-h4 {  
-    padding: 5px 0 5px 30px;
-    background-size: 25px 25px;
-    color: inherit;
-    background-repeat: no-repeat;
-    background-position: left center;
-    margin: 15px 0 10px;
-    display: block;
-    margin-block-start: 1.33em;
-    margin-block-end: 0.33em;
-    margin-inline-start: 0px;
-    margin-inline-end: 0px;
-    font-weight: bold;
-}
-tbody {
-    display: table-row-group;
-    vertical-align: middle;
-    border-color: inherit;
-}
-table tr th {
-    border: 1px solid #B1CEA6;
-    background-color: #F5FCF2;
-}
-table tr td {
-    border: 1px solid #B1CEA6;
-    background-color: #FFF;
-}
-td {
-    display: table-cell;
-    vertical-align: inherit;
-}
-table {
-    border-collapse: collapse;
-    border-spacing: 0;
-}
-.select1{
-    background-color: #dcedc8!important;;
-}
-.select2{
-    background-color: #fff9c4!important;
-}
+@import './style.css';
 </style>
 <script>
 export default {
